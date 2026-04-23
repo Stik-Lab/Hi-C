4ce5c424bc9dc581bc8c5b8018d8d49e07d21816
eferreA9 <157719630+eferreA9@users.noreply.github.com>
2026-04-23 15:40:42 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -10,7 +10,7 @@ path_cooltools='path_cooltools'
 path_cscore='path_cscore'
 
 # Files and programs 
-HICUP_trunc='../bin/hiCUP/HiCUP-0.9.2/hicup_truncater'   # requirements
+HICUP_trunc='hiCUP/HiCUP-0.9.2/hicup_truncater'   # requirements
 indexgenome='/mnt/beegfs/public/references/index/bowtie2/GRCh38_noalt_as/GRCh38_noalt_as'   # requirements
 refgenome='/mnt/beegfs/public/references/genome/human/GRCh38.primary_assembly.genome.fa' 
 ref_compartments='ref/refcool_MRC5_ATAC_100kb.bed'  # requirements
