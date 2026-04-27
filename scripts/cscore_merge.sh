1277efd95445334820cce7cd4ffaf32efadd2be6
eferreA9 <157719630+eferreA9@users.noreply.github.com>
2026-04-27 14:38:49 +0200

    Update cscore_merge.sh

--- scripts/cscore_merge.sh
+++ scripts/cscore_merge.sh
@@ -23,15 +23,19 @@ module load cscoretool/1.1
 
 echo " ................................................................ START cscore ${describer} ................................................................ "
 
-CscoreTool1.1 ${genome100kb} \
-    homertxt_${array_id}.txt \
-    ${path_cscore}/${describer} \
-    4 1000000
+if [ -s ${path_cscore}/${describer}*_tp.bedgraph ]; then
+    echo "SKIP cscore: output already exists for ${describer}"
+else
+        CscoreTool1.1 ${genome100kb} \
+        homertxt_${array_id}.txt \
+        ${path_cscore}/${describer} \
+        4 1000000
+        
+        for i in "${path_cscore}/${describer}"_cscore.bedgraph
+        do
+                outfile=$(basename "$i" _cscore.bedgraph)
+                sed 1d "$i" | awk -v OFS='\t' '$1 != "chrY" {print $1, $2+1, $3, $4}' > "${path_cscore}/${outfile}_tp.bedgraph"
+        done
 
+fi
 echo " ................................................................ END cscore ${describer} ................................................................ "
-
-for i in "${path_cscore}/${describer}"_cscore.bedgraph
-do
-outfile=$(basename "$i" _cscore.bedgraph)
-sed 1d "$i" | awk -v OFS='\t' '$1 != "chrY" {print $1, $2+1, $3, $4}' > "${path_cscore}/${outfile}_tp.bedgraph"
-done
