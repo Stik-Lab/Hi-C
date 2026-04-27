25ae935edca9c9c574a6cc5e8cca916a8175f095
eferreA9 <157719630+eferreA9@users.noreply.github.com>
2026-04-27 15:53:50 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -8,6 +8,7 @@ path_coolMatrix='path_coolMatrix'
 path_loops='path_loops'
 path_cooltools='path_cooltools'
 path_cscore='path_cscore'
+path_juicerEV='path_juicerEV'
 
 # Files and programs 
 HICUP_trunc='hiCUP/HiCUP-0.9.2/hicup_truncater'   # requirements
@@ -23,5 +24,6 @@ restriction_enzyme='^GATC,MboI'
 restrictionSequence='GATC'
 danglingSequence='GATC'
 genome='hg38'
-merge='yes'                 # Enable or disable replicate merging
-numRep=2
+merge='yes/no'                 # Enable or disable replicate merging
+numRep=2/3
+compartments='cscore/juicer/both'
